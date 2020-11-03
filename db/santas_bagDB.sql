36b961740c5fb738cdc2777432c53484d3d16844
samergain <samersadon@gmail.com>
2020-11-03 11:57:35 -0500

    initial files structure and login screen

---
+++ db/santas_bagDB.sql
@@ -0,0 +1,34 @@
+drop database santas_bagDB;
+create database santas_bagDB;
+use santas_bagDB;
+
+create table user (
+	id int auto_increment not null primary key,
+    email varchar(100) not null,
+    password varchar(40)
+);
+
+create table gift (
+	id int auto_increment not null primary key,
+    name varchar(100) not null,
+    category varchar(100),
+    price integer,
+    keywords varchar(200)
+);
+
+create table userCircle (
+	id int auto_increment not null primary key,
+    name varchar(100),
+    age int,
+    interests varchar(200),
+    budget integer,
+    FOREIGN KEY (userId)
+		REFERENCES user(id)
+        ON UPDATE cascade
+        ON DELETE cascade,
+    FOREIGN KEY (giftId)
+		REFERENCES gift(id)
+        ON UPDATE SET NULL
+        ON DELETE SET NULL
+);
+
---
+++ models/gift.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+    var Gift = sequelize.define("Gift", {
+      name: DataTypes.STRING,
+      category: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      keywords: DataTypes.STRING,
+      href: DataTypes.STRING
+    });
+  
+    Gift.associate = function(models) {
+      
+      Gift.hasMany(models.userCircle, {
+        onDelete: "Null"
+      });
+    };
+  
+    return Gift;
+  };
+  
