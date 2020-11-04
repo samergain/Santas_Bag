d144f911bbf1110440645e803c10709a00e49ab5
Meena Ambalam <meena.a.ambalam@gmail.com>
2020-11-04 02:27:19 -0600

    modified front-end

--- public/js/giftPerson.js
+++ public/js/giftPerson.js
@@ -1,36 +1,39 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var addPersonForm = $("form.addPerson");
+  var userId;
   var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
 
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
-    $(".hiddenId").val(data.id);
+    userId = data.id;
+    // findPersons(userId);
+    $(".hiddenId").text(data.id);
   });
 
   // When the signup button is clicked, we validate the email and password are not blank
   addPersonForm.on("submit", function(event) {
     event.preventDefault();
     console.log("search form submit working");
+    var userId = parseInt($(".hiddenId").text());
     var userCircleData = {
       name : $("#nameInput").val().trim(),
       age: $("#ageInput").val().trim(),
       interests: $("#interestsInput").val().trim(),
       budget: $("#priceInput").val().trim(),
-      userid: $("#userId").val().trim()
+      userid: userId
     };
 
     if (!userCircleData.name)  {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    addGiftPerson(userCircleData.name, userCircleData.age, userCircleData.interests, userCircleData.budget, userCircleData.id);
+    addGiftPerson(userCircleData.name, userCircleData.age, userCircleData.interests, userCircleData.budget, userCircleData.userid);
     $("#nameInput").val("");
     $("#ageInput").val("");
     $("#priceInput").val("");
     $("#interests").val("");
-    $("#userId").val("");
+
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
@@ -46,7 +49,8 @@ $(document).ready(function() {
     })
       .then(function(data) {
         console.log("addedGiftPErson", data);
-        window.location.replace("/giftSearch");
+        window.location.replace("/giftPerson");
+        // window.location.replace("/giftSearch");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
