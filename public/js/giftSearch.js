682e7f3bee60b8c183cd6858d40617d582a4803a
Jeff T <jeff.thao92@gmail.com>
2020-11-04 14:59:39 -0600

    Fixed the for loop in displaycards function so that it appends the results. Deleted everything that was commented out, no longer need them.

--- public/js/giftSearch.js
+++ public/js/giftSearch.js
@@ -1,118 +1,108 @@
 $(document).ready(function () {
-  // Getting references to our form and input
-  var searchGift = $("form.searchGift");
-  // var userId;
-
-  // $.get("/api/user_data").then(function(data) {
-  //   email = data.email;
-  //   userId = data.id;
-  // });
-loadOnePersonInfo();
-
-function loadOnePersonInfo(){
-  var data = {
-    getUserDataFromSession: function () {
-      var userData = window.sessionStorage.getItem('userObject');
-      var stringgify = JSON.stringify(userData);
-      console.log(userData);
-      // return JSON.parse(userData);
-      return userData;
-    }
-  }
-  var userDataObject = data.getUserDataFromSession();
-  console.log("userDataSession : ", userDataObject);
-  console.log("userDataSession.userData[0] : ", userDataObject.UserData[0]);
-  console.log("userDataSession.userData[0].id : ", userDataObject.UserData[0].id);
-
-  // for (i = 0; i < userDataObject.UserData.length; i++) {
-    $("#id").text(userDataObject.UserData[0].id);
-        $("#name").text(userDataObject.UserData[0].name);
-        $("#age").text(userDataObject.UserData[0].age);
-        $("#budget").text(userDataObject.UserData[0].budget);
-        $("#interests").text(userDataObject.UserData[0].keywords);
-    // $('div').append('<p><span>' + userDataObject.UserData[i].Terms + ' : </span><span>' + userDataObject.UserData[i].Definitions + '</span></p>')
-  // }
-}
+    // Getting references to our form and input
+    var searchGift = $("form.searchGift");
+    // var userId;
   
-
-  // When the signup button is clicked, we validate the email and password are not blank
-  searchGift.on("submit", function (event) {
-    event.preventDefault();
-    console.log("form submit working");
-    var srchGift = {
-      srchInput: $("#srchInput").val().trim()
-    };
-
-    if (!srchGift.srchInput) {
-      return;
-    }
-    // If we have an email and password, run the signUpUser function
-    getGifts(srchGift.srchInput);
-    $("#srchInput").val("");
-  });
-
-  // Does a post to the signup route. If successful, we are redirected to the members page
-  // Otherwise we log any errors
-  function getGifts(srchGift) {
-    console.log("function getGifts called");
-    $.get("/api/giftSearch/" + srchGift, function (data) {
-      console.log(data);
-      renderGifts(data);
-    });
-  }
-
-  function renderGifts(data) {
-    if (data.length !== 0) {
-
-      $("#srchedResult").empty();
-      $("#srchedResult").show();
-
-      for (var i = 0; i < data.length; i++) {
-
-        var div = $("<div>");
-
-        div.append("<h2>" + data[i].title + "</h2>");
-        div.append("<p>Name: " + data[i].name + "</p>");
-        div.append("<p>Category: " + data[i].category + "</p>");
-        div.append("<p>Price: " + data[i].price + "</p>");
-        div.append("<p>URL Link: " + data[i].href + "</p>");
-        div.append("<button class='select' data-id='" + data[i].id + "'>SELECT GIFT</button>");
-
-        $("#srchedResult").append(div);
-
+    // $.get("/api/user_data").then(function(data) {
+    //   email = data.email;
+    //   userId = data.id;
+    // });
+  loadOnePersonInfo();
+  
+  function loadOnePersonInfo(){
+    var data = {
+      getUserDataFromSession: function () {
+        var userData = window.sessionStorage.getItem('userObject');
+        var stringgify = JSON.stringify(userData);
+        console.log(userData);
+        // return JSON.parse(userData);
+        return userData;
       }
-
-      // $(".select").click(function() {
-
-      //   $.ajax({
-      //     method: "UPDATE",
-      //     url: "/api/personGift/" + $(this).attr("data-id")
-      //   })
-      //     // On success, run the following code
-      //     .then(function() {
-      //       console.log("Gift Added Successfully!");
-      //     });
-
-      //   //$(this).closest("div").remove();
-
-      // });
-
     }
+    var userDataObject = data.getUserDataFromSession();
+    console.log("userDataSession : ", userDataObject);
+    console.log("userDataSession.userData[0] : ", userDataObject.UserData[0]);
+    console.log("userDataSession.userData[0].id : ", userDataObject.UserData[0].id);
+  
+    // for (i = 0; i < userDataObject.UserData.length; i++) {
+      $("#id").text(userDataObject.UserData[0].id);
+          $("#name").text(userDataObject.UserData[0].name);
+          $("#age").text(userDataObject.UserData[0].age);
+          $("#budget").text(userDataObject.UserData[0].budget);
+          $("#interests").text(userDataObject.UserData[0].keywords);
+      // $('div').append('<p><span>' + userDataObject.UserData[i].Terms + ' : </span><span>' + userDataObject.UserData[i].Definitions + '</span></p>')
+    // }
   }
-
-
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
-  }
-});
-
-    d2.append(hFive);
-    d2.append(pTag);
-    $(".result").append(d1);
-}
-
-    d2.append(hFive);
-    d2.append(pTag);
-    $(".result").append(d1);
-}
+    
+  
+    // When the signup button is clicked, we validate the email and password are not blank
+    searchGift.on("submit", function (event) {
+      event.preventDefault();
+      console.log("form submit working");
+      var srchGift = {
+        srchInput: $("#srchInput").val().trim()
+      };
+  
+      if (!srchGift.srchInput) {
+        return;
+      }
+      // If we have an email and password, run the signUpUser function
+      getGifts(srchGift.srchInput);
+      $("#srchInput").val("");
+    });
+  
+    // Does a post to the signup route. If successful, we are redirected to the members page
+    // Otherwise we log any errors
+    function getGifts(srchGift) {
+      console.log("function getGifts called");
+      $.get("/api/giftSearch/" + srchGift, function (data) {
+        console.log(data);
+        renderGifts(data);
+      });
+    }
+  
+    function renderGifts(data) {
+      if (data.length !== 0) {
+  
+        $("#srchedResult").empty();
+        $("#srchedResult").show();
+  
+        for (var i = 0; i < data.length; i++) {
+  
+          var div = $("<div>");
+  
+          div.append("<h2>" + data[i].title + "</h2>");
+          div.append("<p>Name: " + data[i].name + "</p>");
+          div.append("<p>Category: " + data[i].category + "</p>");
+          div.append("<p>Price: " + data[i].price + "</p>");
+          div.append("<p>URL Link: " + data[i].href + "</p>");
+          div.append("<button class='select' data-id='" + data[i].id + "'>SELECT GIFT</button>");
+  
+          $("#srchedResult").append(div);
+  
+        }
+  
+        // $(".select").click(function() {
+  
+        //   $.ajax({
+        //     method: "UPDATE",
+        //     url: "/api/personGift/" + $(this).attr("data-id")
+        //   })
+        //     // On success, run the following code
+        //     .then(function() {
+        //       console.log("Gift Added Successfully!");
+        //     });
+  
+        //   //$(this).closest("div").remove();
+  
+        // });
+  
+      }
+    }
+  
+  
+    function handleLoginErr(err) {
+      $("#alert .msg").text(err.responseJSON);
+      $("#alert").fadeIn(500);
+    }
+  });
--- public/js/giftSearchcreate.js
+++ public/js/giftSearchcreate.js
@@ -1,5 +1,3 @@
-console.log("hello")
-
 let giftObj = {
     title: [],
     thumbnail: [],
@@ -10,9 +8,9 @@ let giftObj = {
 $("#submitBtn").click(function (event) {
     event.preventDefault();
     console.log("inside");
-    var gift = $(this).attr("data-gift");
+    let gift = $(this).attr("data-gift");
     console.log(gift);
-    var giftItems = $("#srchInput").val().trim();
+    let giftItems = $("#srchInput").val().trim();
 
     const settings = {
         "async": true,
@@ -20,61 +18,49 @@ $("#submitBtn").click(function (event) {
         "url": `https://rapidapi.p.rapidapi.com/product/search?keyword=${giftItems}&country=US`,
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "6d1e8c65b3msh1a27a65b7342b1dp10f81fjsn7715a836924d",
+            "x-rapidapi-key": "",
             "x-rapidapi-host": "amazon-product-reviews-keywords.p.rapidapi.com"
         }
     };
 
     $.ajax(settings).done(function (response) {
-        // console.log(response)
-        for (let i = 0; (i < response.products.length
-            && i < 10
-        ); i++) {
-               const giftInfo = response.products[i];
-               console.log(response.products[i]);
-            //console.log(response.products[i].title);
+        for (let i = 0; (i < response.products.length && i < 10); i++) {
+            const giftInfo = response.products[i];
+            console.log(giftItems);
             giftObj.title.push(response.products[i].title);
-            // console.log(response.products[i].thumbnail);
             giftObj.thumbnail.push(response.products[i].thumbnail);
-            // console.log(response.products[i].price.current_price);
             giftObj.current_price.push("$" + response.products[i].price.current_price);
-            // console.log(response.products[i].url);
             giftObj.url.push(response.products[i].url);
         }
         displayCards(giftObj)
-        // console.log(response.products[0].title);
-        // console.log(response.products[0].thumbnail);
-        // console.log(response.products[0].price.current_price);
-        // console.log(response.products[0].url);
-
     });
-
 });
 
 function displayCards(giftObj) {
-
     console.log(giftObj)
+     for (let i = 0; i < giftObj.thumbnail.length; i++) {
 
-    let d1 = $("<div>");
-    d1.attr("class", "card");
-    d1.attr("style", "width: 100%;");
-    let d2 = $("<div>");
-    d2.attr("class", "card-body");
-    let img = $("<img>");
-    img.attr("class","card-img-top");
-    img.attr("src", giftObj.thumbnail[0]);
-    let aTag = $("<a>");
-    aTag.attr("href", giftObj.url[0]);
-    let hFive = $("<h5>");
-    hFive.attr("class", "card-title");
-    hFive.text(giftObj.title[0]);
-    let pTag = $("<p>");
-    pTag.attr("class", "card-text");
-    pTag.text(giftObj.current_price[0]);
+        let d1 = $("<div>");
+        d1.attr("class", "card");
+        d1.attr("style", "width: 100%;");
+        let d2 = $("<div>");
+        d2.attr("class", "card-body");
+        let img = $("<img>");
+        img.attr("class", "card-img-top");
+        img.attr("src", giftObj.thumbnail[i]);
+        let aTag = $("<a>");
+        aTag.attr("href", giftObj.url[i]);
+        let hFive = $("<h5>");
+        hFive.attr("class", "card-title");
+        hFive.text(giftObj.title[i]);
+        let pTag = $("<p>");
+        pTag.attr("class", "card-text");
+        pTag.text(giftObj.current_price[i]);
 
-    d1.append(d2);
-    d1.append(img);
-    d2.append(hFive);
-    d2.append(pTag);
-    $(".result").append(d1);
+        d1.append(d2);
+        d1.append(img);
+        d2.append(hFive);
+        d2.append(pTag);
+        $(".result").append(d1);
+    }   
 }
