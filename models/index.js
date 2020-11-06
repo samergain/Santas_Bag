d6132ac1dc0387078b205798fa178a26f2caaa44
samergain <samersadon@gmail.com>
2020-11-05 20:45:25 -0500

    setting the Heroku connection

--- models/index.js
+++ models/index.js
@@ -5,14 +5,12 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
+var config    = require(__dirname + '/../config/config.js')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  console.log("line 12 in index.js");
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  console.log("line 14 in index.js", config);
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
